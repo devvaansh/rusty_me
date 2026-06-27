3c7ca94635566e4cec25b1829fa849db20889311
Devansh Vashisht <devansh.vashisht.ug24@nsut.ac.in>
2026-06-27 16:12:00 +0530

    feat: report per-line warnings in normalize_logfmt example

--- examples/normalize_logfmt.rs
+++ examples/normalize_logfmt.rs
@@ -1,12 +1,19 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let document = rusty_me::parse_document_strict(input.trim_end())?;
+    let (document, errors) = rusty_me::parse_document_lossy(input.trim_end());
     let normalized = document.encode();
     println!("{normalized}");
+
+    if !errors.is_empty() {
+        let mut stderr = io::stderr().lock();
+        for error in errors {
+            writeln!(stderr, "warning: {error}")?;
+        }
+    }
 
     Ok(())
 }
